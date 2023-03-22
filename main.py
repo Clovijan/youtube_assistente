7f04e4af4f33fde1f125928ad170acc99cc30a36
Clovijan Rocha <clovijan@gmail.com>
2023-03-21 22:49:03 -0300

    Add do método que traduz o texto

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from googletrans import Translator
+
+# A partir do ID do vídeo ele recupera as legendas desse vídeo
 
 
-# A partir do ID do vídeo ele recupera as legendas de um vídeo
-
-def getcaption(idVideo):
+def getCaption(idVideo):
     transcript = YouTubeTranscriptApi.get_transcript(
         idVideo, languages=['pt'])
-    print(transcript)
     return transcript
+
+
+def tradutor(transcript):
+    translator = Translator()
+    translated = translator.translate(transcript, src="pt", dest='en').text
+    return translated
+
+
+print(tradutor('ola mundo'))
